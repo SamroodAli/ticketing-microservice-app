30d064c5a82ed1902013bf66bec10b0c2b021fb9
samroodAli <devsamrood@gmail.com>
2021-11-02 13:27:44 +0530

    Auth:CurrentUser dummy handler in routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,15 @@
-import express from 'express'
-import { json, urlencoded } from "body-parser"
+import express from "express";
+import { json, urlencoded } from "body-parser";
 
-const app = express()
-app.use(json())
-app.use(urlencoded({ extended: true }))
+import { currentUserRouter } from "./routes/current-user";
+
+const app = express();
+app.use(json());
+
+app.use(urlencoded({ extended: true }));
+
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
