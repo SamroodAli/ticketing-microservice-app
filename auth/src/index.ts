6e166b35160fc183df2e97b4c9c6e58af1b51248
samroodAli <devsamrood@gmail.com>
2021-11-02 14:55:58 +0530

    Auth:Handle errors using error handling middleware with four arguments

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/singout";
 import { signupRouter } from "./routes/signup";
 
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(json());
 
@@ -15,6 +17,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
