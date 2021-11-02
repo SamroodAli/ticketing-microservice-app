b9714c6f7828e078e5fcd13b04158bc069a3ea4f
samroodAli <devsamrood@gmail.com>
2021-11-02 13:33:26 +0530

    Auth:Import route handlers in routes in auth index.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/singout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
