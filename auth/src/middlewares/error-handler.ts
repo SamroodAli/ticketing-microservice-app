d36abe85ce7d426ce8db68eca145af3b5674e783
samroodAli <devsamrood@gmail.com>
2021-11-03 15:54:55 +0530

    Auth:Error handler checks for instance of customError instead of various errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -9,13 +8,10 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   console.log("something went wrong", err);
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).json({ errors: err.serializeErrors() });
-  }
   res.status(400).send({
     errors: [{ message: "Somethig went wrong " }],
   });
