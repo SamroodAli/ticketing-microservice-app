3eab59b69bad6bbf237c9f890e52671b9a1b3aea
samroodAli <devsamrood@gmail.com>
2021-11-04 11:12:44 +0530

    Auth:Compare passwords static method for class method using scrypt

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -10,5 +10,10 @@ export class Password {
 
     return `${buffer.toString("hex")}.${salt}`;
   }
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split(".");
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buffer.toString("hex") === hashedPassword;
+  }
 }
