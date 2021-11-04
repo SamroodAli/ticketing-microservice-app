5b6ade69eb214e990bf0ecf0781f3c724d26f495
samroodAli <devsamrood@gmail.com>
2021-11-04 10:03:24 +0530

    Auth:Give user model typescript type information using generics

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,14 +1,23 @@
 import mongoose from "mongoose";
 
-// An interface that describes the properties that are required to create a new User
+// An interface that describes the properties
+// that are required to create a new User
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-// An interface that describes the properties that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,6 +35,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
