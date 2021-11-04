b8daf00d45bf167edd3af42d4a6a16d82c87ba12
samroodAli <devsamrood@gmail.com>
2021-11-04 09:47:52 +0530

    Typescript:Add static method to userSchema with typescript types and information

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
+// An interface that describes the properties that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +22,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
 
 export { User };
