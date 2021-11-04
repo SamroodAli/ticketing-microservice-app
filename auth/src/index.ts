bc9b56f1ad6612d1c1ef67db4c71745ca52f6df5
samroodAli <devsamrood@gmail.com>
2021-11-04 08:41:34 +0530

    Mongodb:Connect to mongodb by creating a pod and via a clusterip service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json, urlencoded } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -26,6 +27,16 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Auth server is running on port 3000");
+  });
+};
+
+start();
