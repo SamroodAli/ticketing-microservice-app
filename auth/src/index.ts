4ae86327fadd4095c2a29159cc76ffb817de5b2a
samroodAli <devsamrood@gmail.com>
2021-11-08 15:07:26 +0530

    Auth:setup cookie session in cookie auth service and set trust proxy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json, urlencoded } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -12,8 +13,15 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found";
 
 const app = express();
-app.use(json());
+app.set("trust proxy", true); // traffic is proxied from ingress-nginx
 
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(urlencoded({ extended: true }));
 
 app.use(currentUserRouter);
