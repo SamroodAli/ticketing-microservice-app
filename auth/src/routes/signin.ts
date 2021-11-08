6e345e37e9d9151958d6bda35409953fef04dad9
samroodAli <devsamrood@gmail.com>
2021-11-08 18:25:41 +0530

    Auth:Create new middleware for refactoring request validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("hey sigin");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email"),
+    body("password").trim().notEmpty().withMessage("Please supply a password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    // send response
+  }
+);
 
 export { router as signinRouter };
