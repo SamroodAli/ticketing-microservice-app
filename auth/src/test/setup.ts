3ff27b6543bf9b73a4143ccad9589ef6a5c50bb9
samroodAli <devsamrood@gmail.com>
2021-11-09 17:31:59 +0530

    Test: Auth write test for successful signup

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import { app } from "../app";
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
   await mongoose.connect(mongoUri, {});
