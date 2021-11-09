8926461e53225d2e1960252f514f2669f8223ecd
samroodAli <devsamrood@gmail.com>
2021-11-09 17:25:44 +0530

    Auth:Setup test and setup test file with jest,supertest and in memory mongodb

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,23 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
