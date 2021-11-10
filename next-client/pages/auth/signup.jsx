9255e26e81634e12db131c2e667218da720173d7
samroodAli <devsamrood@gmail.com>
2021-11-10 16:01:24 +0530

    Client:Sign up form make request and show errors on sign up

--- next-client/pages/auth/signup.jsx
+++ next-client/pages/auth/signup.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
+import axios from "axios";
 
 const SignupForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.error(err.response.data);
+      setErrors(err.response.data.errors);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign up </h1>
       <div className="form-group">
         <label htmlFor="emailAddress">Email Address</label>
@@ -25,6 +41,16 @@ const SignupForm = () => {
           className="form-control"
         />
       </div>
+      {errors.length && (
+        <div className="alert alert-danger">
+          <h4>Sorry, please provide valid crendentials</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
