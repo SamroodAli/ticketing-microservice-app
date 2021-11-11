866184f582a72b20c137a31826c73ef16deee4aa
samroodAli <devsamrood@gmail.com>
2021-11-11 19:19:58 +0530

    Fix: Fix import for link in header

--- next-client/pages/_app.js
+++ next-client/pages/_app.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build-client";
+import Header from "../components/Header";
 
+console.log(Header);
 function MyApp({ Component, pageProps, currentUser }) {
   return (
     <>
-      <h1>Hi {currentUser.email}</h1>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />
     </>
   );
