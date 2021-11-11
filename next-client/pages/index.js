29084e42064b07ae39051dfc88423c8a22fe5cee
samroodAli <devsamrood@gmail.com>
2021-11-11 17:41:21 +0530

    Client:GetServerside props with request to auth for current user route

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <div> Welcome to ticketing.dev</div>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  return <div>{JSON.stringify(currentUser)}</div>;
+};
+
+export async function getServerSideProps() {
+  // url can be "http://auth-srv:3000/api/users/currentuser"
+  console.log("I am in server");
+  try {
+    const response = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        { headers: { Host: "ticketing.dev" } } // for ingress, as set host to ticketing.dev
+      )
+      .catch(console.error);
+
+    return {
+      props: { currentUser: response.data.currentUser },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: { currentUser: null },
+    };
+  }
 }
+
+export default LandingPage;
