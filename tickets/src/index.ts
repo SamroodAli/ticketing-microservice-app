f06e3b6168cba1887d5ea8073e3853a6cccfd3d0
samroodAli <devsamrood@gmail.com>
2021-11-12 20:29:01 +0530

    Tickets:Change hardcoded mongo uri string in app to a env variable from tickets depl

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,9 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI JWT_KEY must be defined");
+  }
 
   try {
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to tickets service MongoDb");
   } catch (err) {
     console.error(err);
