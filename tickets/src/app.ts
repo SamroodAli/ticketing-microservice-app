396501098da9706fde1b8a4b8d9f292ac1925bbf
samroodAli <devsamrood@gmail.com>
2021-11-12 20:10:06 +0530

    Tickets:Copy code from auth for new tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import { json, urlencoded } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@devstoic-learning/ticketing";
+
+const app = express();
+app.set("trust proxy", true); // traffic is proxied from ingress-nginx
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", //true unless running tests
+  })
+);
+app.use(urlencoded({ extended: true }));
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,43 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+
+let mongo: MongoMemoryServer;
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  return mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+  // dont await to avoid jest timing out
+});
+
+global.signin = async () => {
+  const email = "globaltest@test.com";
+  const password = "pasword";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
