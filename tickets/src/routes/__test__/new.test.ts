fc8fbf6f6b675d1ad9abf66e54c58dd59503b11a
samroodAli <devsamrood@gmail.com>
2021-11-15 15:52:24 +0530

    Test: Create test for new ticket

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/Ticket";
 
 it("has a route handler listening to /api/tickets for a post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -61,7 +62,10 @@ it("returns an error if an invalid price is provided", async () => {
 
 it("creates a ticket with valid inputs", async () => {
   // add in check to make sure a ticket was saved
-  const response = request(app)
+  let tickets = await Ticket.find({}).lean();
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
@@ -69,5 +73,11 @@ it("creates a ticket with valid inputs", async () => {
       price: 20,
     })
     .expect(201);
-  return response;
+
+  tickets = await Ticket.find({}).lean();
+  expect(tickets.length).toEqual(1);
+
+  const ticket = tickets[0];
+  expect(ticket.title).toEqual("validTitle");
+  return expect(ticket.price).toEqual(20);
 });
