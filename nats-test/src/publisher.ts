85184db4f68ea3e5148c8f1dbfba558ca489451d
samroodAli <devsamrood@gmail.com>
2021-11-17 18:10:33 +0530

    Nats:Make publish function in base class Publisher return a promise

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,21 +1,24 @@
 import nats from "node-nats-streaming";
+import { TicketPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
-}); //stan is client
+}); //stan is alias for a client which is the inverse of nats
 
 stan.on("connect", () => {
-  console.log("Publisher connected to NATS");
+  //stan is alias for a client which is the inverse of nats
+  console.log(
+    "Publisher connected to NA//stan is alias for a client which is the inverse of natsTS"
+  );
 
-  const event = JSON.stringify({
+  const event = {
     id: "123",
     title: "concert",
     price: 20,
-  });
+  };
 
-  stan.publish("ticket:created", event, () => {
-    console.log("published");
-  });
+  const publisher = new TicketPublisher(stan);
+  publisher.publish(event);
 });
