fa3d0c6d7c1ab41547002221950d36c50b48e4a6
samroodAli <devsamrood@gmail.com>
2021-11-18 19:13:55 +0530

    Ticket:Add env variables to ticket deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,7 +7,7 @@ const start = async () => {
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI JWT_KEY must be defined");
+    throw new Error("MONGO_URI must be defined");
   }
 
   try {
