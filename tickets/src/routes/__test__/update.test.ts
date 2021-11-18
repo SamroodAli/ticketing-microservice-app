ce4e63350db95a21bac0b35b214788082086578a
samroodAli <devsamrood@gmail.com>
2021-11-18 19:04:46 +0530

    Ticket:Test with jest mock function publishing to ticket updated event

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
-import { Ticket } from "../../models/Ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
   const id = global.newMongooseId();
@@ -89,4 +89,29 @@ it("updates the ticket if valid credentials are given", async () => {
 
   expect(ticketResponse.body.title).toEqual("new title");
   expect(ticketResponse.body.price).toEqual("100");
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({ title: "Old title", price: 20 });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "new title",
+      price: 100,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
