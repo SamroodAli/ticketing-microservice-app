70491de511b459f338877b02c3eeac293bd80831
samroodAli <devsamrood@gmail.com>
2021-11-18 14:45:02 +0530

    Tickets:Create getter for getting client stan from natsWrapper

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -3,6 +3,13 @@ import nats, { Stan } from "node-nats-streaming";
 class NatsWrapper {
   private _client?: Stan; // question mark tells typescript this is going to be undefined for some time
 
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+    return this._client;
+  }
+
   //cluserId that we want to connect to, the client name, url of the bus to connect
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
