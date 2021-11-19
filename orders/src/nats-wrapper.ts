03e06d4ffc568d81d4d977fccf4a86436bdc168d
samroodAli <devsamrood@gmail.com>
2021-11-19 14:44:37 +0530

    Orders:App with server and mongodb logic, index js with starter and nats-wrapper

---
+++ orders/src/nats-wrapper.ts
@@ -0,0 +1,29 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan; // question mark tells typescript this is going to be undefined for some time
+
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+    return this._client;
+  }
+
+  //cluserId that we want to connect to, the client name, url of the bus to connect
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log(`${clientId} connected to nats`);
+        resolve();
+      });
+      this.client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
