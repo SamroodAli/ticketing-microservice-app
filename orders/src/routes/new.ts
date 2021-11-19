317fb3598c0b871d74d8f7153d0aead86e845801
samroodAli <devsamrood@gmail.com>
2021-11-19 16:50:19 +0530

    Orders:Write create post route for creating new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,25 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@devstoic-learning/ticketing";
+import { body } from "express-validator";
 
 const router = express.Router();
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => {
+        mongoose.Types.ObjectId.isValid(input);
+      })
+      .withMessage("Please provide a valid ticket Id"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
