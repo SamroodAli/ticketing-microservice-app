380d8f2e881cfa331f948f3cab0751f277ac6a96
samroodAli <devsamrood@gmail.com>
2021-11-22 18:04:51 +0530

    Orders:Test new route creation of order

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,53 @@
+import request from "supertest"
+import { app } from "../../app"
+import mongoose from "mongoose"
+import { Ticket } from "../../models/Ticket"
+import {Order,OrderStatus} from "../../models/Order"
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  return request(app)
+    .post("/api/orders")
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404)
+})
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price:20
+  })
+
+  await ticket.save()
+  const order = Order.build({
+    ticket,
+    userId: "Sadasdasdasdasd",
+    status: OrderStatus.Created,
+    expiresAt:new Date()
+  })
+
+  await order.save()
+
+  return request(app)
+    .post('/api/orders')
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400)
+
+})
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price:20
+  })
+
+  await ticket.save()
+  return request(app)
+    .post('/api/orders')
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+})
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -21,9 +21,7 @@ router.post(
     body("ticketId")
       .not()
       .isEmpty()
-      .custom((input: string) => {
-        mongoose.Types.ObjectId.isValid(input);
-      })
+      .custom((input: string) =>  mongoose.Types.ObjectId.isValid(input))
       .withMessage("Please provide a valid ticket Id"),
   ],
   validateRequest,
@@ -33,6 +31,7 @@ router.post(
     const { ticketId } = req.body;
     // Make sure that this ticket is not already reserved
     const ticket = await Ticket.findById(ticketId);
+
     if (!ticket) {
       throw new NotFoundError();
     }
@@ -44,8 +43,10 @@ router.post(
     }
     // Calculate an expiration date for this order
     const expiration = new Date()
+
     expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
     // Build the order and save it to the database
+
     const order = Order.build({
       userId: req.currentUser!.id,
       status: OrderStatus.Created,
