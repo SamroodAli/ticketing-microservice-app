58d080bd6ddfb4af93edb31998b70650a46c9825
samroodAli <devsamrood@gmail.com>
2021-11-23 15:20:15 +0530

    Orders:Ticket updated listener class in orders service

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,31 @@
+import { Message } from "node-nats-streaming";
+import {
+  Subjects,
+  Listener,
+  TicketUpdatedEvent,
+} from "@devstoic-learning/ticketing";
+import { Ticket } from "../../models/Ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    const { title, price } = data;
+
+    ticket.set({
+      title,
+      price,
+    });
+
+    await ticket.save();
+    msg.ack();
+  }
+}
