992ee0a74643b69fd8470fdc7360a0dd00aab5bb
samroodAli <devsamrood@gmail.com>
2021-11-26 11:44:03 +0530

    Tickets:Reserve a ticket on order created event

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,13 +5,25 @@ import {
 } from "@devstoic-learning/ticketing";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/Ticket";
-import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
-import { natsWrapper } from "../../nats-wrapper";
 import { queueGroupName } from "./queue-group-name";
 
 class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    // if no ticket,throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    // mark the ticket as being reserved
+    ticket.set({ orderId: data.id });
+    //save the ticket
+    await ticket.save();
+    msg.ack();
+  }
 }
+
+export { OrderCreatedListener };
