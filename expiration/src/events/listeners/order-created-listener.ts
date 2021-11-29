a024bc8a31bfacec13968d22ffca015df9c8490c
samroodAli <devsamrood@gmail.com>
2021-11-29 20:08:30 +0530

    Expiration:Listen to order creation events and add a job to the bull queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,15 @@ import {
 } from "@devstoic-learning/ticketing";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // adding a job to the queue on order creation
+    await expirationQueue.add({ orderId: data.id });
+    msg.ack();
+  }
 }
