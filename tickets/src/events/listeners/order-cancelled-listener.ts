49cc60d603c7057f0674311c22ad282852f029c8
samroodAli <devsamrood@gmail.com>
2021-11-29 15:32:29 +0530

    Order:Cancelled listener that publishes ticket updated event

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,36 @@
+import {
+  Listener,
+  OrderCancelledEvent,
+  Subjects,
+} from "@devstoic-learning/ticketing";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/Ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
