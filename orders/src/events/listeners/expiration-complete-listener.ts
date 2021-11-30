96b923785ef8b6c4a66739868125c8f400dda403
samroodAli <devsamrood@gmail.com>
2021-11-30 15:07:52 +0530

    Orders:Cancel order on expiration complete event and publish order cancel event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,13 +2,34 @@ import {
   Listener,
   ExpirationCompleteEvent,
   Subjects,
+  OrderStatus,
 } from "@devstoic-learning/ticketing";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Order } from "../../models/Order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
   readonly queueGroupName = queueGroupName;
 
-  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {}
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId).populate("ticket").exec();
+    if (!order) {
+      throw new Error("Order not found");
+    }
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    await order.save();
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+    msg.ack();
+  }
 }
