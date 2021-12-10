2f9fa2e9b9a1664cc7943c2acaf526c7d28320c3
samroodAli <devsamrood@gmail.com>
2021-12-10 13:24:30 +0530

    Payments:Create payments service boilterplate setup

---
+++ payments/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import "express-async-errors";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cookieSession from "cookie-session";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@devstoic-learning/ticketing";
+
+const app = express();
+app.set("trust proxy", true); // traffic is proxied from ingress-nginx
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(morgan("dev"));
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", //true unless running tests
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,55 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+let mongo: MongoMemoryServer;
+
+declare global {
+  var signin: () => string[];
+  var newMongooseId: () => string;
+}
+
+jest.mock("../nats-wrapper.ts");
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  return mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+  // dont await to avoid jest timing out
+});
+
+global.newMongooseId = () => new mongoose.Types.ObjectId().toHexString();
+
+global.signin = () => {
+  // build a jwt payload {id,email}
+  const payload = {
+    id: global.newMongooseId(),
+    email: "test@test.com",
+  };
+
+  // create a jwt token
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  // build session object { jwt : my_jwt }
+  const session = { jwt: token };
+  // turn that session into JSON as express sends it as JSON
+  const sessionJSON = JSON.stringify(session);
+  // take JSON and encode it as base64 as  cookieSession packages encodes it in base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  // return a string thats the cookie with the encoded data
+  //supertest expects cookies in an array
+  return [`express:sess=${base64}`];
+};
