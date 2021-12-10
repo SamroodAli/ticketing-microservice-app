69ea6442de1b21dae5922efa0d7add1eaad9fc5e
samroodAli <devsamrood@gmail.com>
2021-12-10 14:22:26 +0530

    Payments:Create order model to track orders inside payments service

---
+++ payments/src/models/Order.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@devstoic-learning/ticketing";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  // id is already in mongoose.Document
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        //ret is returning document
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", OrderSchema);
