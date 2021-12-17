af042765fbd327f9236a43bc1735a60bba915685
samroodAli <devsamrood@gmail.com>
2021-12-17 19:39:07 +0530

    Orders:Create order updated event publisher in case it is needed for future

---
+++ orders/src/events/publishers/order-updated-publisher.ts
@@ -0,0 +1,9 @@
+import {
+  Publisher,
+  OrderUpdatedEvent,
+  Subjects,
+} from "@devstoic-learning/ticketing";
+
+export class OrderUpdatedPublisher extends Publisher<OrderUpdatedEvent> {
+  readonly subject = Subjects.OrderUpdatedEvent;
+}
