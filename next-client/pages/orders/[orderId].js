b5974747ab8b66d929cc6ea0e5e12e3bf329900a
samroodAli <devsamrood@gmail.com>
2021-12-21 00:37:05 +0530

    Client:Order show page to show time left till order cancellation using setInterval in useEffect with order expiresAt property

--- next-client/pages/orders/[orderId].js
+++ next-client/pages/orders/[orderId].js
@@ -1,4 +1,41 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>;
+import { useEffect, useState } from "react";
+import buildClient from "../../api/build-client";
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const minutesLeft = new Date(order.expiresAt) - new Date();
+      const secondsLeft = Math.round(minutesLeft / 1000);
+      setTimeLeft(secondsLeft);
+    };
+    // call immediately to show immediately
+    findTimeLeft();
+    // set interval
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return <div>{timeLeft}</div>;
 };
+
+export const getServerSideProps = async (context) => {
+  try {
+    const { orderId } = context.params;
+    const { data } = await buildClient(context).get(`/api/orders/${orderId}`);
+    return {
+      props: {
+        order: data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {},
+    };
+  }
+};
+
 export default OrderShow;
