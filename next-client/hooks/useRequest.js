a8c9879ed7036d168bcad57fc2692cadf272f605
samroodAli <devsamrood@gmail.com>
2021-12-21 21:15:44 +0530

    Client:Create two new routes for signed in users, to new tickets and show orders pages

--- next-client/hooks/useRequest.js
+++ next-client/hooks/useRequest.js
@@ -4,25 +4,30 @@ import { useState } from "react";
 const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url, body);
+      const response = await axios[method](url, {
+        ...body,
+        ...props,
+      });
       if (onSuccess) {
         onSuccess(response.data);
       }
       return response.data;
     } catch (err) {
       const errors = err.response.data.errors;
-      setErrors(
-        <div className="alert alert-danger">
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      );
+      if (Array.isArray(errors)) {
+        setErrors(
+          <div className="alert alert-danger">
+            <ul className="my-0">
+              {errors.map((err) => (
+                <li key={err.message}>{err.message}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      }
     }
   };
 
--- next-client/pages/orders/[orderId].js
+++ next-client/pages/orders/[orderId].js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import buildClient from "../../api/build-client";
 import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/useRequest";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -30,9 +39,12 @@ const OrderShow = ({ order }) => {
     <div>
       <p>Time left to pay {timeLeft}</p>
       <StripeCheckout
-        token={(token) => console.log(token)}
+        token={(token) => doRequest({ token: token.id })} //token is an object with metadata on the token which has id property which is the string token we need
         stripeKey={process.env.NEXT_PUBLIC_STRIPE_KEY}
+        amount={order.ticket.price * 100} // dollar to cents
+        email={currentUser.email}
       />
+      {errors}
     </div>
   );
 };
